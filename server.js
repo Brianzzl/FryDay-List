078bb1cca0509b3873fc3c5758003054a0cc0eff
Yating(Esther) Su <yaringsu@foxmail.com>
2019-12-03 14:47:24 -0500

    Set up Server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set up Heroku
+if (process.env.NODE_ENV === "priducton") {
+  app.use(express.static("client/build"));
+}
+
+//connect to MongoDB
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("connection success .....");
+  })
+  .catch(err => console.error(err));
+
+//start the Api server
+
+app.listen(PORT, () => {
+  console.log("Listing......" + PORT);
+});
