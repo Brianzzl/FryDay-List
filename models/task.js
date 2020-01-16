a8884559927919e06596ad21b6bf7856c728715a
Yating(Esther) Su <yaringsu@foxmail.com>
2020-01-16 12:10:37 -0500

    bugfix, in Task.js

--- models/task.js
+++ models/task.js
@@ -1,16 +1,14 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
+const moogoose = require("mongoose");
+const Schema = moogoose.Schema;
 
 const taskSchema = new Schema({
   taskName: { type: String, require: true },
   dueDate: { type: Date },
   category: { type: String, require: true },
-  status: { type: String, default: "inprogress" },
-  comments: { type: String },
+  status: { type: String, default: "Inprogress" },
+  comment: { type: String },
   weather: { type: String, require: true }
 });
 
-const Tasks = mongoose.model("Tasks", taskSchema);
-
-module.exports = Tasks;
+const Task = mongoose.model("Task", taskSchema);
+module.export = Task;
